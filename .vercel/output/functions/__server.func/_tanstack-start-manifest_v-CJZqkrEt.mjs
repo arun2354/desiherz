20b378aa7f366c534eace51ec834a9c0decf80d4
arun2354 <arun.goten@gmail.com>
2026-06-08 02:02:28 +0200

    Delete .vercel/output directory

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CJZqkrEt.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/tmp/cc-agent/67628160/project/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-C13wahwI.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-C13wahwI.js" } }] }, "/": { filePath: "/tmp/cc-agent/67628160/project/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BPs3bzdp.js"] } } });
-export {
-  tsrStartManifest
-};
